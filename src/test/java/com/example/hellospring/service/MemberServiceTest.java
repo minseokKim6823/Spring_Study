ec5c075db5655943949b693a277cc8c8306f04d4
minseokkim6823 <alstjr6823@naver.com>
2023-05-06 17:48:24 +0900

    DI 홈화면 추가

--- src/test/java/com/example/hellospring/service/MemberServiceTest.java
+++ src/test/java/com/example/hellospring/service/MemberServiceTest.java
@@ -1,14 +1,68 @@
 package com.example.hellospring.service;
 
-import org.junit.jupiter.api.Test;
+import com.example.hellospring.domain.Member;
+import com.example.hellospring.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.*;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.*;
 
 class MemberServiceTest {
 
-    @Test
-    void join() {
+    MemberService memberService;
+    MemoryMemberRepository memberRepository;
+
+    @BeforeEach
+    public void beforeEach(){
+        memberRepository = new MemoryMemberRepository();
+        memberService =new MemberService(memberRepository);
     }
+
+    @AfterEach
+    public void afterEach(){
+        memberRepository.clearStore();//메서드가 끝날 때마다 저장소를 지워줌
+    }
+    @Test
+    void 회원가입() {
+        //given
+        Member member=new Member();
+        member.setName("spring");
+        //when
+        Long saveId= memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertThat(member.getName()).isEqualTo(findMember.getName());
+    }
+    @Test
+    public void 중복_회원_예외(){
+        //given
+        Member member1 =new Member();
+        member1.setName("spring");
+
+        Member member2 =new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e =assertThrows(IllegalStateException.class, () ->memberService.join(member2));
+
+        assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.");
+
+/*
+        try{
+            memberService.join(member2);
+            fail();
+        }catch (IllegalStateException e){
+            assertThat(e.getMessage()).isEqualTo("이미 존재하는 회원입니다.123123");
+        }
+*/
+
+        //then
+    }
+
 
     @Test
     void findmembers() {
