c5b9f84127e41f7f2bdf69f32c10538e94336340
minseokkim6823 <alstjr6823@naver.com>
2023-05-07 02:03:51 +0900

    JPA1

--- src/main/java/com/example/hellospring/SpringConfig.java
+++ src/main/java/com/example/hellospring/SpringConfig.java
@@ -1,9 +1,8 @@
 package com.example.hellospring;
 
-import com.example.hellospring.repository.JdbcMemberRepository;
-import com.example.hellospring.repository.MemberRepository;
-import com.example.hellospring.repository.MemoryMemberRepository;
+import com.example.hellospring.repository.*;
 import com.example.hellospring.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,14 +11,11 @@ import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
-
-    private DataSource dataSource;
-
+    private EntityManager em;
     @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em){
+        this.em=em;
     }
-
     @Bean //스프링 빈을 등록하겠다는 의미
     public MemberService memberService(){
         return new MemberService(memberRepository());
@@ -28,6 +24,7 @@ public class SpringConfig {
     @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
-        return new JdbcMemberRepository(dataSource);
+        //return new JdbcMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
